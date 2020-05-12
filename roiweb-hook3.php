2e35f6ca5ec5707d97c8bfbd451b6166d9f4c21e
roiwebco <30891172+roiwebco@users.noreply.github.com>
2020-05-12 10:16:28 +0200

    Update roiweb-hook3.php

--- roiweb-hook3.php
+++ roiweb-hook3.php
@@ -9,25 +9,21 @@ Version: 1.0.0
 */
 //Exit if accessed directly
 if (!defined('ABSPATH'))exit;
-//HOOK3
-
-//[laptop] shortcode
-function wzr_mobile_shortcode($atts, $content = null){ 
+//[laptop]shortcode
+function rwc_mobile_shortcode($atts, $content = null){ 
   if( !wp_is_mobile() ){ 
     return wpautop( do_shortcode( $content ) ); 
   } else {
     return null; 
   } 
 }
-add_shortcode('laptop', 'wzr_mobile_shortcode');
-
-//[mobile] shortcode
-
-function wzr_laptop_shortcode($atts, $content = null){ 
+add_shortcode('laptop', 'rwc_mobile_shortcode');
+//[mobile]shortcode
+function rwc_laptop_shortcode($atts, $content = null){ 
   if( wp_is_mobile() ){ 
     return  wpautop( do_shortcode( $content ) ); 
   } else {
     return null; 
   } 
 }
-add_shortcode('mobile', 'wzr_laptop_shortcode');
+add_shortcode('mobile', 'rwc_laptop_shortcode');
